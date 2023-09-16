1a5de0025d739b202a6d4e3c035a347cde201388
Yiweiwangau <81563224+Yiweiwangau@users.noreply.github.com>
2023-09-17 00:59:53 +1000

    add checkbox on multi select; fix cursor bug; reset filter; refactor duplication

--- src/api/dogs.ts
+++ src/api/dogs.ts
@@ -1,27 +1,35 @@
 import api from './index';
 
-export const getBreeds = () => {
+export const getBreeds = async () => {
   return api.get('/dogs/breeds');
 }
 
-export const searchDogs = (breeds: string[] = [], zipCodes: string[] = [], ageMin?: number, ageMax?: number, size: number = 25, from?: string, sort?: string) => {
-  return api.get('/dogs/search', {
-    params: {
-      breeds,
-      zipCodes,
-      ageMin,
-      ageMax,
-      size,
-      from,
-      sort
-    }
-  });
-}
+export const searchDogs = async (
+    breeds: string[] = [],
+    zipCodes: string[] = [],
+    ageMin?: number,
+    ageMax?: number,
+    size: number = 25,
+    from?: string,
+    sort?: string
+) => {
+    return api.get("/dogs/search", {
+        params: {
+            breeds,
+            zipCodes,
+            ageMin,
+            ageMax,
+            size,
+            from,
+            sort,
+        },
+    });
+};
 
-export const getDogs = (dogIds: string[]) => {
+export const getDogs = async (dogIds: string[]) => {
   return api.post('/dogs', dogIds);
 }
 
-export const matchDogs = (dogIds: string[]) => {
+export const matchDogs = async (dogIds: string[]) => {
   return api.post('/dogs/match', dogIds);
 }
--- src/pages/DogSearchPage.tsx
+++ src/pages/DogSearchPage.tsx
@@ -14,25 +14,56 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogActions from "@mui/material/DialogActions";
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
+import Checkbox from "@mui/material/Checkbox";
+import ListItemText from "@mui/material/ListItemText";
 import DogList from "../components/DogList/DogList";
 import { Dog } from "../types/types";
 import { getBreeds, searchDogs, getDogs, matchDogs } from "../api/dogs";
 import CustomPagination from "../components/Pagination/CustomPagination";
 
+type AgeSelectorProps = {
+    age: number | undefined;
+    setAge: React.Dispatch<React.SetStateAction<number | undefined>>;
+    label: string;
+};
+
+const AgeSelector: React.FC<AgeSelectorProps> = ({ age, setAge, label }) => (
+    <>
+        <Typography variant="subtitle1">{label}:</Typography>
+        <Select
+            value={age ? age : ""}
+            onChange={(e) => setAge(Number(e.target.value))}
+            style={{ minWidth: "100px", marginRight: "10px" }}
+        >
+            <MenuItem value="">Any</MenuItem>
+            {Array.from({ length: 21 }).map((_, index) => (
+                <MenuItem key={index} value={index}>
+                    {index} {index === 1 ? "Year" : "Years"}
+                </MenuItem>
+            ))}
+        </Select>
+    </>
+);
+
 const DogSearchPage: React.FC = () => {
     const [breeds, setBreeds] = useState<string[]>([]);
-    const [selectedBreed, setSelectedBreed] = useState<string | undefined>();
     const [dogs, setDogs] = useState<Dog[]>([]);
     const [favorites, setFavorites] = useState<Dog[]>([]);
     const [cursors, setCursors] = useState<string[]>([]);
-    const [currentCursor, setCurrentCursor] = useState<string | undefined>();
+    const [requestedCursor, setRequestedCursor] = useState<
+        string | undefined
+    >();
     const [nextCursor, setNextCursor] = useState<string | undefined>();
     const [prevCursor, setPrevCursor] = useState<string | undefined>();
     const [matchedDog, setMatchedDog] = useState<Dog | null>(null);
     const [isMatchDialogOpen, setIsMatchDialogOpen] = useState<boolean>(false);
-    const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc");
     const [favoritesDialogOpen, setFavoritesDialogOpen] =
         useState<boolean>(false);
+    const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc");
+    const [selectedBreeds, setSelectedBreeds] = useState<string[]>([]);
+    const [selectedZipCodes, setSelectedZipCodes] = useState<string[]>([]);
+    const [ageMin, setAgeMin] = useState<number | undefined>();
+    const [ageMax, setAgeMax] = useState<number | undefined>();
 
     useEffect(() => {
         async function fetchBreeds() {
@@ -67,41 +98,68 @@ const DogSearchPage: React.FC = () => {
 
     useEffect(() => {
         async function fetchDogs() {
+            const sortQuery = `breed:${sortOrder}`;
             const response = await searchDogs(
-                selectedBreed ? [selectedBreed] : [],
-                [],
-                undefined,
-                undefined,
+                selectedBreeds,
+                selectedZipCodes,
+                ageMin,
+                ageMax,
                 25,
-                nextCursor
+                requestedCursor,
+                sortQuery
             );
-            console.log(response);
+            console.log(response.data);
 
             setNextCursor(getCursorFromQueryString(response.data.next));
             setPrevCursor(getCursorFromQueryString(response.data.prev));
 
-            if (response.data.resultIds.length > 0) {
-                const dogDetails = await getDogs(response.data.resultIds);
-                setDogs(dogDetails.data);
-                console.log(dogDetails.data);
-            }
+            const dogDetails = await getDogs(response.data.resultIds);
+            setDogs(dogDetails.data);
         }
         fetchDogs();
-    }, [selectedBreed, currentCursor]);
+    }, [
+        selectedBreeds,
+        selectedZipCodes,
+        ageMin,
+        ageMax,
+        requestedCursor,
+        sortOrder,
+    ]);
 
-    const handleBreedChange = (event: SelectChangeEvent<string>) => {
-        setSelectedBreed(event.target.value);
+    useEffect(() => {
+        if (ageMin && ageMax && ageMin > ageMax) {
+            setAgeMax(ageMin);
+        }
+    }, [ageMin]);
+
+    useEffect(() => {
+        if (ageMax && ageMin && ageMax < ageMin) {
+            setAgeMin(ageMax);
+        }
+    }, [ageMax]);
+
+    const handleBreedsChange = (event: SelectChangeEvent<string[]>) => {
+        if (event.target.value.includes("Remove Selection")) {
+            setSelectedBreeds([]);
+        } else {
+            setSelectedBreeds(event.target.value as string[]);
+        }
     };
 
-    const sortedDogs = [...dogs].sort((a, b) => {
-        const comparison = a.breed.localeCompare(b.breed);
-        return sortOrder === "asc" ? comparison : -comparison;
-    });
+    const handleZipCodesChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        if (event.target.value.trim() === "") {
+            setSelectedZipCodes([]);
+        } else {
+            setSelectedZipCodes([event.target.value.trim()]);
+        }
+    };
 
     const handleNext = () => {
         if (nextCursor) {
             setCursors((prev) => [...prev, nextCursor]);
-            setCurrentCursor(nextCursor);
+            setRequestedCursor(nextCursor);
         }
     };
 
@@ -111,9 +169,9 @@ const DogSearchPage: React.FC = () => {
                 const newCursors = [...cursors];
                 newCursors.pop();
                 setCursors(newCursors);
-                setCurrentCursor(newCursors[newCursors.length - 1]);
+                setRequestedCursor(newCursors[newCursors.length - 1]);
             } else {
-                setCurrentCursor(undefined);
+                setRequestedCursor(undefined);
                 setCursors([]);
             }
         }
@@ -154,22 +212,58 @@ const DogSearchPage: React.FC = () => {
         }
     };
 
+    const resetFilters = () => {
+        setSelectedBreeds([]);
+        setSelectedZipCodes([]);
+        setAgeMin(undefined);
+        setAgeMax(undefined);
+        setSortOrder("asc");
+    };
+
     return (
         <Container>
             <Box className="flex items-center space-x-2">
-                <Typography variant="subtitle1">Breed:</Typography>
+                <Typography variant="subtitle1">Breeds:</Typography>
                 <Select
-                    value={selectedBreed || ""}
-                    onChange={handleBreedChange}
+                    multiple
+                    value={selectedBreeds}
+                    onChange={handleBreedsChange}
+                    renderValue={(selected) =>
+                        (selected as string[]).join(", ")
+                    }
                     style={{ minWidth: "150px" }}
                 >
+                    <MenuItem value="Remove Selection">
+                        <Checkbox
+                            checked={
+                                selectedBreeds.length === breeds.length ||
+                                selectedBreeds.includes("Remove Selection")
+                            }
+                        />
+                        <ListItemText primary="Remove Selection" />
+                    </MenuItem>
                     {breeds.map((breed) => (
                         <MenuItem key={breed} value={breed}>
-                            {breed}
+                            <Checkbox
+                                checked={selectedBreeds.includes(breed)}
+                            />
+                            <ListItemText primary={breed} />
                         </MenuItem>
                     ))}
                 </Select>
 
+                <Typography variant="subtitle1">Zip Code:</Typography>
+                <input
+                    type="text"
+                    value={selectedZipCodes}
+                    onChange={handleZipCodesChange}
+                    placeholder="Enter Zip Code"
+                    style={{ marginRight: "10px", padding: "5px" }}
+                />
+
+                <AgeSelector age={ageMin} setAge={setAgeMin} label="Age Min" />
+                <AgeSelector age={ageMax} setAge={setAgeMax} label="Age Max" />
+                <Button onClick={resetFilters}>Reset Filters</Button>
                 <Typography variant="subtitle1">Sort By:</Typography>
                 <Select
                     value={sortOrder}
@@ -237,11 +331,15 @@ const DogSearchPage: React.FC = () => {
                 </DialogContent>
             </Dialog>
 
-            <DogList
-                dogs={sortedDogs}
-                favorites={favorites}
-                onFavorite={toggleFavorite}
-            />
+            {dogs.length === 0 ? (
+                <Typography variant="h6">No dogs are available</Typography>
+            ) : (
+                <DogList
+                    dogs={dogs}
+                    favorites={favorites}
+                    onFavorite={toggleFavorite}
+                />
+            )}
 
             <CustomPagination
                 onNext={handleNext}
